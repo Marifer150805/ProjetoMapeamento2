20823be50c713e5b56cae5f4a15511a6f7eeddf9
Maria Fernanda <132359722+Marifer150805@users.noreply.github.com>
2024-03-14 14:36:25 -0300

    Add files via upload

---
+++ projetoMapeamento3/src/main/java/com/example/demo/ProjetoMapeamento3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMapeamento3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMapeamento3Application.class, args);
+	}
+
+}
